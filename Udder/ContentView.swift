f393648be89e3caf546311c83e3a7d54cebea41e
“TomislavPiskur” <tomislav.piskur@gmail.com>
2023-12-29 17:41:15 +0100

    Added image and imageView.

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -1,30 +1,11 @@
-//
-//  ContentView.swift
-//  Udder
-//
-//  Created by Tomek on 29.12.2023..
-//
-
 import SwiftUI
+
 
 struct ContentView: View {
     var body: some View {
         VStack {
             Label("paperplane", systemImage: "paperplane")
-            Label("paperplane", systemImage: "paperplane")
-
-            Label("paperplane", systemImage: "paperplane")
-
-            Label("paperplane", systemImage: "paperplane")
-
-            Label("paperplane", systemImage: "paperplane")
-
-            Label("paperplane", systemImage: "paperplane")
-
-            Label("paperplane", systemImage: "paperplane")
-
-            Label("paperplane", systemImage: "paperplane")
-
+            Image("cow")
         }
         .padding()
     }
