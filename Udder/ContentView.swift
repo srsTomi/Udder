7e8fe19c31ebd347ff9b234f4c5e48454d0ad5c0
“TomislavPiskur” <tomislav.piskur@gmail.com>
2023-12-29 17:30:20 +0100

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Tomek on 29.12.2023..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Tomek on 29.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
