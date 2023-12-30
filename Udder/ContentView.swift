9291a0642f36a7fda1d07673cd447425d9dc2ec6
“TomislavPiskur” <tomislav.piskur@gmail.com>
2023-12-30 12:36:38 +0100

    Added duplicate label

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -5,11 +5,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Label("paperplane", systemImage: "paperplane")
+            Label("paperplane", systemImage: "paperplane")
             Image("cow")
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
